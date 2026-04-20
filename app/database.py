7776509e7f148080986f296c5fe13cc8deda4592
Mohamed Mufeed <mufeedbinismail@gmail.com>
2026-04-20 16:09:02 +0530

    feat: added support for postgresql and mssql

--- app/database.py
+++ app/database.py
@@ -1,17 +1,39 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
+from urllib.parse import quote_plus
 import os
 
 load_dotenv()
 
-DB_URL = "mysql+pymysql://{user}:{password}@{host}:{port}/{name}".format(
+_DRIVERS = {
+    "mariadb":     "mysql+pymysql",
+    "mysql":       "mysql+pymysql",
+    "postgresql":  "postgresql+psycopg2",
+    "mssql":       "mssql+pyodbc",
+}
+
+_db_engine = os.getenv("DB_ENGINE", "mariadb").lower()
+_driver = _DRIVERS.get(_db_engine)
+
+if not _driver:
+    raise ValueError(
+        f"Unsupported DB_ENGINE '{_db_engine}'. "
+        f"Supported values: {', '.join(_DRIVERS.keys())}"
+    )
+
+DB_URL = "{driver}://{user}:{password}@{host}:{port}/{name}".format(
+    driver=_driver,
     user=os.getenv("DB_USER", "root"),
-    password=os.getenv("DB_PASSWORD", ""),
+    password=quote_plus(os.getenv("DB_PASSWORD", "")),
     host=os.getenv("DB_HOST", "127.0.0.1"),
     port=os.getenv("DB_PORT", "3306"),
     name=os.getenv("DB_NAME", "zkteco_sync"),
 )
+
+if _db_engine == "mssql":
+    odbc_driver = os.getenv("DB_ODBC_DRIVER", "ODBC Driver 17 for SQL Server")
+    DB_URL += f"?driver={quote_plus(odbc_driver)}"
 
 engine = create_engine(DB_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
