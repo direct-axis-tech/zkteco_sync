37efb86ea113cc5d4d5ea920e97c8a10f705f781
Mohamed Mufeed <mufeedbinismail@gmail.com>
2026-04-20 15:06:31 +0530

    feat: made the app listen & port configurable

--- run.py
+++ run.py
@@ -1,4 +1,13 @@
 import uvicorn
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(
+        "app.main:app",
+        host=os.getenv("APP_HOST", "0.0.0.0"),
+        port=int(os.getenv("APP_PORT", "8000")),
+        reload=os.getenv("APP_ENV", "production") == "development",
+    )
